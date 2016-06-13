1842a9809b34b1c4783e2eeefef3f520e56875a4
Seaborn Lee <seabornlee.cn@gmail.com>
2016-06-13 10:30:00 +0800

    Add @Override

--- src/main/java/cn/thoughtworks/academy/marsrover/Direction.java
+++ src/main/java/cn/thoughtworks/academy/marsrover/Direction.java
@@ -2,37 +2,45 @@ package cn.thoughtworks.academy.marsrover;
 
 public enum Direction {
     WEST() {
+        @Override
         public Direction left() {
             return SOUTH;
         }
 
+        @Override
         public Direction right() {
             return NORTH;
         }
     },
     SOUTH() {
+        @Override
         public Direction left() {
             return EAST;
         }
 
+        @Override
         public Direction right() {
             return WEST;
         }
     },
     EAST() {
+        @Override
         public Direction left() {
             return NORTH;
         }
 
+        @Override
         public Direction right() {
             return SOUTH;
         }
     },
     NORTH() {
+        @Override
         public Direction left() {
             return WEST;
         }
 
+        @Override
         public Direction right() {
             return EAST;
         }
